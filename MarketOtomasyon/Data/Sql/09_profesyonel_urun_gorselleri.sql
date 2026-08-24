baeaa023188b673cc8dcf79996843822b07d2f13
Taha <cicektahayasin46@gmail.com>
2026-08-24 11:04:42 +0300

    feat: replace flat product icons with professional catalog images

---
+++ MarketOtomasyon/Data/Sql/09_profesyonel_urun_gorselleri.sql
@@ -0,0 +1,25 @@
+/*
+    Profesyonel urun katalog gorsellerini Urun kayitlarina baglar.
+    Dosyalar wwwroot/urun-gorsel/URN001.webp - URN030.webp altindadir.
+*/
+
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+
+BEGIN TRANSACTION;
+
+UPDATE Urun
+SET ResimYolu = N'/urun-gorsel/' + Kod + N'.webp',
+    ResimKaynagi = NULL,
+    ResimTarihi = SYSUTCDATETIME()
+WHERE Kod LIKE N'URN[0-9][0-9][0-9]'
+  AND TRY_CONVERT(INT, RIGHT(Kod, 3)) BETWEEN 1 AND 30;
+
+IF @@ROWCOUNT <> 30
+BEGIN
+    ROLLBACK TRANSACTION;
+    THROW 51000, 'URN001-URN030 arasindaki 30 urunun tamami bulunamadi.', 1;
+END;
+
+COMMIT TRANSACTION;
+
